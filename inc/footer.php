769f01f7716dbeae21023cf674a556c8fe8b237e
Neo Futur <cahier2@ww7.pe>
2012-05-19 07:34:43 +0200

    replaced casimir with gwgd

--- inc/footer.php
+++ inc/footer.php
@@ -6,7 +6,7 @@
         <a href="<?php echo $casimir->base_url; ?>">Home</a> |
         <a href="<?php echo $casimir->base_url; ?>tools.php">Tools</a> |
         <a href="<?php echo $casimir->base_url; ?>stats.php#lastday">Stats</a> |
-        <a href="http://wiki.github.com/nhoizey/casimir">About Cas.im/ir</a>
+        <a href="https://github.com/neofutur/gwgd">About gwgd shortener</a>
       </p>
     </div>
     <?php
